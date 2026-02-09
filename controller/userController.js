d17ff9c63c3a31e2db957cd2f299ef231d3eb390
suyog-max <suyogbhandari93@gmail.com>
2026-02-09 13:51:36 +0545

    first commit

---
+++ controller/userController.js
@@ -0,0 +1,7 @@
+const User = require('../model/userModel');
+ const register = async(req,res) => {
+ }
+
+ const login = async(req,res) => { 
+ }
+ module.exports = {register,login};
---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("Hello, World!");
+//today we will study about MVC clean architecture and publish in github
+
+//at first, we will learn about userside
